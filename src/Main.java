500a5036777c2165426bb46b75bced9ec566c0ff
almunimax <almunimax@mail.ru>
2022-11-30 19:47:24 +0300

    задание 6 общий вес и разница веса

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // задание 6
+        System.out.println("задание 6");
+        var boxer1Weight = 78.2;
+        var boxer2Weight = 82.7;
+        System.out.println("общий вес двух бойцов " + (boxer1Weight + boxer2Weight));
+        System.out.println("разница весов двух бойцов " + (boxer2Weight - boxer1Weight));
+    }
+}
